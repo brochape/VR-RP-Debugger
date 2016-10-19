58a6cbce853a4e87420c86f36806d8c50125302f
Bruno Rocha Pereira <brochape@ulb.ac.be>
2016-10-19 21:36:34 +0200

    [Add] Added the first Unity project

---
+++ Debugger/Assets/Spawn.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using Noesis;
+using Noesis.Javascript;
+
+public class NewBehaviourScript : MonoBehaviour {
+	private TextMesh textMesh;
+	// Use this for initialization
+	void Start () {
+		JavascriptContext context = new JavascriptContext ();
+		StreamReader reader = File.OpenText("elmAST.js");
+		string line;
+		line = reader.ReadToEnd ();
+		Debug.Log (line);
+		var Text = new GameObject();
+		textMesh = Text.AddComponent<TextMesh>();
+		textMesh.font = Resources.Load("calibrili") as Font;
+		textMesh.text = line;
+		textMesh.transform.Translate (Vector3.up*10);
+
+		context.Run (line);
+		context.Dispose ();
+		//		Instantiate(textMesh, new Vector3(10, 10, 10), Quaternion.identity);
+
+		//		int i = 0;
+		//		while ((line = reader.ReadLine()) != null) {
+		//			string[] items = line.Split(' ');
+		//			if (items [0].Length != 0) {
+		//				Debug.Log (items [0]);
+		//				var Text = new GameObject();
+		//				var textMesh = Text.AddComponent<TextMesh>();
+		//				textMesh.font = Resources.Load("calibrili") as Font;
+		//				textMesh.text = items [0];
+		//				Instantiate(textMesh, new Vector3(i, 0, 0), Quaternion.identity);
+		//				i += 10;
+		////				switch (items [0]) {
+		////				case 
+		//				}
+		//			}
+
+		//			int myInteger = int.Parse(items[1]); // Here's your integer.
+		//			// Now let's find the path.
+		//			string path = null;
+		//			foreach (string item in items) {
+		//				if (item.StartsWith("item\\") && item.EndsWith(".ddj")) {
+		//					path = item;
+		//				}
+		//			}
+
+		// At this point, `myInteger` and `path` contain the values we want
+		// for the current line. We can then store those values or print them,
+		// or anything else we like.
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
