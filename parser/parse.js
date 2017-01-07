ffad8d7a7d6a5f4fc5503c978dde9f1ff648d6b9
Bruno Rocha Pereira <b.rocha.per@gmail.com>
2017-01-07 18:21:07 +0100

    [Add] parse example

--- parser/parse.js
+++ parser/parse.js
@@ -4,3 +4,6 @@ var peg = require('pegjs')
 var syntax = fs.readFileSync("syntax.js").toString();
 
 var parser = peg.generate(syntax);
+
+parser.parse("var signalTest = Signal(9)\n\
+              var boolTest = True or False or True")
