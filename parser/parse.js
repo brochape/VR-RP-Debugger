8b2d36c7e3f28ba21a4283136a948ca2962cd98b
Bruno Rocha Pereira <b.rocha.per@gmail.com>
2017-02-26 21:04:39 +0100

    [WIP] Interpreter

--- parser/parse.js
+++ parser/parse.js
@@ -14,9 +14,34 @@ var parser = peg.generate(syntax);
 
 
 var ast = parser.parse("activate seconds\nfold(seconds + 0)\nfilter((a) => ( a > 0 ) 0 numbers)")//\nvar signalTest = Signal(90)\nvar boolTest = true || false || true\nvar stringTest = 'coucou'
-ast = ast;
-console.log(ast[1][0].children)
+ast = ast.map(([x, y]) => x);
+console.log(ast[0])
 
+var secondSignal = false;
+
+var signalGraph = {
+    name: "root",
+    children: []
+}
+
+
+for (var i = 0; i < ast.length; i++) {
+    var statement = ast[i];
+    switch(statement.name){
+        case "signalActivation":
+            switch(statement.children[0].value){
+                case "seconds":
+                    secondSignal = true;
+                    var secondSignal = {
+                        name: "secondSignal",
+                        value: 0,
+                        children: []
+                    }
+                    signalGraph.children.push(secondSignal)
+            }
+    }   
+}
+console.log(signalGraph);
 
 var indent = 1;
 function walk(tree) {
@@ -30,3 +55,5 @@ function walk(tree) {
         }
     })
 }
+
+
