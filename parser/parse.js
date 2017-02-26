18500edfb2ace65c8cb72f197ca74238a67f7a86
Bruno Rocha Pereira <b.rocha.per@gmail.com>
2017-02-26 22:32:24 +0100

    [WIP] Fold interpreter

--- parser/parse.js
+++ parser/parse.js
@@ -15,7 +15,7 @@ var parser = peg.generate(syntax);
 
 var ast = parser.parse("activate seconds\nfold(seconds + 0)\nfilter((a) => ( a > 0 ) 0 numbers)")//\nvar signalTest = Signal(90)\nvar boolTest = true || false || true\nvar stringTest = 'coucou'
 ast = ast.map(([x, y]) => x);
-console.log(ast[0])
+// console.log(ast[0])
 
 var secondSignal = false;
 
@@ -29,19 +29,39 @@ for (var i = 0; i < ast.length; i++) {
     var statement = ast[i];
     switch(statement.name){
         case "signalActivation":
-            switch(statement.children[0].value){
+            var signalName = statement.children[0].value;
+            switch(signalName){
                 case "seconds":
                     secondSignal = true;
                     var secondSignal = {
-                        name: "secondSignal",
+                        name: signalName,
                         value: 0,
                         children: []
                     }
-                    signalGraph.children.push(secondSignal)
+                    signalGraph["seconds"] = secondSignal;
+                    break;
             }
+            break;
+        case "fold":
+            [signal,operand,initVal] = statement.children.map((x)=>(x.value));
+            var signalValue = signalGraph[signal].value;
+            var formula = signalValue + operand + initVal;
+            var initVal = eval(formula);
+            var behaviourNode = {
+                name: "operation",
+                value: initVal,
+                formula: [signal,operand,initVal]
+            }
+            signalGraph[signal].children.push(behaviourNode);
+            break;
+
     }   
 }
-console.log(signalGraph);
+console.log(signalGraph.seconds.children[0]);
+
+// function findValueForSignal(tree,signal){
+//     return tree.children.filter((el)=>(el.name == signal))[0].value;
+// }
 
 var indent = 1;
 function walk(tree) {
