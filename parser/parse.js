f79abaa1501d1ed3ebc6b8e4fd055d12b4595c7f
Bruno Rocha Pereira <b.rocha.per@gmail.com>
2017-03-01 11:31:00 +0100

    [Ref] Module

--- parser/parse.js
+++ parser/parse.js
@@ -3,134 +3,127 @@ var peg = require('pegjs')
 
 
 // console.log(ast[0])
-function parse(codeString) {
-    var syntax = fs.readFileSync("syntax.pegjs").toString();
+module.exports = {
+    parse: function(codeString) {
+        var syntax = fs.readFileSync("syntax.pegjs").toString();
 
-    var parser = peg.generate(syntax);
-
-
-    //
-    var ast = parser.parse(codeString)//\nvar signalTest = Signal(90)\nvar boolTest = true || false || true\nvar stringTest = 'coucou'
-    ast = ast.map(([x, y]) => x);
-
-    return ast;
-}
-
-function interprete(ast){
-    var signalGraph = {
-        name: "root",
-        children: []
-    }
-
-    for (var i = 0; i < ast.length; i++) {
-        var statement = ast[i];
-        switch(statement.name){
-            case "signalActivation":
-                var signalName = statement.children[0].value;
-                switch(signalName){
-                    case "seconds":
-                        secondSignalEnabled = true;
-                        var secondSignal = {
-                            name: signalName,
-                            value: 0,
-                            children: []
-                        }
-                        signalGraph["seconds"] = secondSignal;
-                        break;
-                }
-                break;
-
-            case "fold":
-                [signal,operand,initVal] = statement.children.map((x)=>(x.value));
-                var signalValue = signalGraph[signal].value;
-                var formula = "<signalValue>" + operand + "currentValue" ;
-                var initVal = eval(formula.replace("<signalValue>",signalValue).replace("currentValue",initVal));
-                var signalNode = {
-                    name: "fold",
-                    value: initVal,
-                    formula: formula
-                }
-                signalGraph[signal].children.push(signalNode);
-                break;
-
-            case "map":
-                [lambda, signal] = statement.children;
-                signal = signal.value;
-                // console.log(lambda);
-
-                var body = lambda.children[1];
-                var param = lambda.children[0].value;
-                var signalValue = signalGraph[signal].value;
-
-                var initVal = eval(body.replace(param,signalValue))
-
-                var signalNode = {
-                    name: "map",
-                    value: initVal,
-                    formula: [body,param]
-                }
-                signalGraph[signal].children.push(signalNode);
-                break;
-
-            case "filter":
-                [lambda, initValue, signal] = statement.children;
-                [initValue,signal] = [initValue,signal].map((x)=>(x.value));
-
-                var body = lambda.children[1];
-                var param = lambda.children[0].value;
-                var signalValue = signalGraph[signal].value;
-
-                var behaviourNode = {
-                    name: "filter",
-                    value: initValue,
-                    formula: [body, param]
-                }
-                signalGraph[signal].children.push(behaviourNode);
-                break;
+        var parser = peg.generate(syntax);
 
 
-        }   
-    }
-    return signalGraph;
-    
-}
+        //
+        var ast = parser.parse(codeString)//\nvar signalTest = Signal(90)\nvar boolTest = true || false || true\nvar stringTest = 'coucou'
+        ast = ast.map(([x, y]) => x);
 
-function handle_signals(signalGraph) {
-    if (secondSignalEnabled) {
-    setInterval(function(){ 
-        signalGraph[signal].value ++;
-        for (var i = 0; i < signalGraph.seconds.children.length; i++) {
-            node = signalGraph.seconds.children[i];
-            switch(node.name){
-                case "fold":
-                    node.value = eval(node.formula.replace("<signalValue>",signalGraph[signal].value).replace("currentValue",node.value));
-                    break;
-                case "map":
-                    [body,param] = node.formula;
-                    node.value = eval(body.replace(param, signalGraph[signal].value));
-                    break;
-                case "filter":
-                    [body,param] = node.formula;
-                    signalValue = signalGraph[signal].value
-                    if (eval(body.replace(param, signalValue))) {
-                        node.value = signalValue;
+        return ast;
+    },
+
+    interprete: function(ast){
+        var signalGraph = {
+            name: "root",
+            children: []
+        }
+
+        for (var i = 0; i < ast.length; i++) {
+            var statement = ast[i];
+            switch(statement.name){
+                case "signalActivation":
+                    var signalName = statement.children[0].value;
+                    switch(signalName){
+                        case "seconds":
+                            secondSignalEnabled = true;
+                            var secondSignal = {
+                                name: signalName,
+                                value: 0,
+                                children: []
+                            }
+                            signalGraph["seconds"] = secondSignal;
+                            break;
                     }
+                    break;
 
-            }
-            console.log(node);
-         } //code goes here that will be run every 5 seconds.    
-    }, 1000);
+                case "fold":
+                    [signal,operand,initVal] = statement.children.map((x)=>(x.value));
+                    var signalValue = signalGraph[signal].value;
+                    var formula = "<signalValue>" + operand + "currentValue" ;
+                    var initVal = eval(formula.replace("<signalValue>",signalValue).replace("currentValue",initVal));
+                    var signalNode = {
+                        name: "fold",
+                        value: initVal,
+                        formula: formula
+                    }
+                    signalGraph[signal].children.push(signalNode);
+                    break;
+
+                case "map":
+                    [lambda, signal] = statement.children;
+                    signal = signal.value;
+                    // console.log(lambda);
+
+                    var body = lambda.children[1];
+                    var param = lambda.children[0].value;
+                    var signalValue = signalGraph[signal].value;
+
+                    var initVal = eval(body.replace(param,signalValue))
+
+                    var signalNode = {
+                        name: "map",
+                        value: initVal,
+                        formula: [body,param]
+                    }
+                    signalGraph[signal].children.push(signalNode);
+                    break;
+
+                case "filter":
+                    [lambda, initValue, signal] = statement.children;
+                    [initValue,signal] = [initValue,signal].map((x)=>(x.value));
+
+                    var body = lambda.children[1];
+                    var param = lambda.children[0].value;
+                    var signalValue = signalGraph[signal].value;
+
+                    var behaviourNode = {
+                        name: "filter",
+                        value: initValue,
+                        formula: [body, param]
+                    }
+                    signalGraph[signal].children.push(behaviourNode);
+                    break;
+
+
+            }   
+        }
+        return signalGraph;
+        
+    },
+
+    handle_signals: function(signalGraph) {
+        if (secondSignalEnabled) {
+        setInterval(function(){ 
+            signalGraph[signal].value ++;
+            for (var i = 0; i < signalGraph.seconds.children.length; i++) {
+                node = signalGraph.seconds.children[i];
+                switch(node.name){
+                    case "fold":
+                        node.value = eval(node.formula.replace("<signalValue>",signalGraph[signal].value).replace("currentValue",node.value));
+                        break;
+                    case "map":
+                        [body,param] = node.formula;
+                        node.value = eval(body.replace(param, signalGraph[signal].value));
+                        break;
+                    case "filter":
+                        [body,param] = node.formula;
+                        signalValue = signalGraph[signal].value
+                        if (eval(body.replace(param, signalValue))) {
+                            node.value = signalValue;
+                        }
+
+                }
+             }   
+        }, 1000);
+        }
+    }
 }
-}
 
-var code = "activate seconds\nmap( (a)=>(a+1) seconds)\nfold(seconds + 0)\nfilter((a)=>(a%3==0) seconds 0)"
-ast = parse(code);
-
-
-
-var secondSignal = false;
-signalGraph = interprete(ast);
-handle_signals(signalGraph);
 
 
 
