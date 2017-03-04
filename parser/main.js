a56cf572fda01308c0f25adb73198cfe438f871b
Bruno Rocha Pereira <b.rocha.per@gmail.com>
2017-03-04 22:59:06 +0100

    [WIP] Variable assignment example

--- parser/main.js
+++ parser/main.js
@@ -6,7 +6,7 @@ const exec = require('child_process').exec;
 
 var parser = require('./parse.js');
 
-var code = "activate seconds\nvar mapVar = map( (a)=>(a+1) seconds)\nfold(seconds + 0)\nfilter((a)=>(a%3==0) seconds 0)"
+var code = "activate seconds\nvar mapVar = map( (a)=>(a+1) seconds)\nfold(seconds + 0)\nfilter((a)=>(a%3==0) seconds 0)\nmap((a)=>(a+1) mapVar)"
 ast = parser.parse(code);
 // console.log(ast);
 
