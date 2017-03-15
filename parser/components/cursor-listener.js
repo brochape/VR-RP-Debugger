406d5b340368221c87226c6027262160576cb65e
Bruno ROCHA PEREIRA <brochape@vub.ac.be>
2017-03-15 13:28:58 +0100

    [Add] Node deletion

--- parser/components/cursor-listener.js
+++ parser/components/cursor-listener.js
@@ -3,10 +3,10 @@ AFRAME.registerComponent('cursor-listener', {
         var visible = 0;
         var counter = 0;
         var COLORS = ['blue','white'];
-        var menuElements = ["fold", "+", "-", "0", "merge","","","","map","","","","filter","","","X"]
+        var menuElements = ["fold", "+", "-", "0", "merge","","","","map","","","","filter","","","DEL"]
         this.el.addEventListener('click', function (evt) {
             counter ++;
-            console.log("click");
+            //console.log("click");
             if (counter%2 == 1) {
                 var randomIndex = Math.floor(Math.random() * COLORS.length);
                 this.setAttribute('material', 'color', COLORS[visible%COLORS.length]);
@@ -28,19 +28,20 @@ AFRAME.registerComponent('cursor-listener', {
 
                     for (var i = 0; i < 4; i++) {
                       for (var j = 0; j < 4; j++) {
-                        var button = document.createElement('a-plane');
+                        var button = document.createElement('a-entity');
                         menu.appendChild(button);
                         menu.setAttribute("position",pos);
+                        button.setAttribute("geometry","primitive: plane; width: 0.4; height:0.4;")
                         button.setAttribute("class","menu-button");
-                        button.setAttribute("text","color: white;font: https://cdn.aframe.io/fonts/Roboto-msdf.json; value:"+menuElements[i*4+j]+";")
+                        button.setAttribute("text","color: white; zOffset: 0.01; align: center; width:2; height:2; value:"+menuElements[i*4+j]+";")
 
                         /*button.setAttribute("text", )*/
-                        button.setAttribute("color", "black");
+                        button.setAttribute("material", " color: #496FFF");
                         if (i*4+j == 15) {
-                            button.setAttribute("color", "red");
+                            button.setAttribute("material", " color: red");
                         }
-                        button.setAttribute("height",0.4);
-                        button.setAttribute("width",0.4);
+                        //button.setAttribute("height",0.4);
+                        //button.setAttribute("width",0.4);
                         newPos = {
                           x: -0.75 + (i*0.5),
                           y: 0.75 - (j*0.5),
@@ -50,9 +51,22 @@ AFRAME.registerComponent('cursor-listener', {
                         button.addEventListener('click',function (evt) {
                             var action = this.getAttribute('text').value;
                             switch(action){
-                                case "X":
-                                    that.parentNode.removeChild(that);
-                                    //TODO: update the tree
+                                case "DEL":
+                                    console.log("I'm deleting",that.id);
+                                    var node = findNodeByID(signalGraph,that.id);
+                                    for (var i = 0; i < node.parents.length; i++) {
+                                        var parentNode = findSignalNode(signalGraph,node.parents[i]);
+                                        console.log(parentNode)
+                                        var array = parentNode.children;
+                                        var index = array.indexOf(node);
+                                        if (index > -1) {
+                                            array.splice(index, 1);
+                                        }
+                                        parentNode.children = array;
+                                        
+                                        that.parentNode.removeChild(that);
+                                    }
+                                    //TODO: delete all depending children?
                             }
                             // This works!
                             //
@@ -71,7 +85,7 @@ AFRAME.registerComponent('cursor-listener', {
                 }
 
                 visible = 1 - visible;
-                console.log(counter);
+                //console.log(counter);
                 
             }
     });
