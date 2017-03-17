fd8d661e14381f6b2e19af9e1adbcfe82dc96f2d
Bruno ROCHA PEREIRA <brochape@vub.ac.be>
2017-03-17 13:43:48 +0100

    [Add] HTML decoding

--- parser/helpers.js
+++ parser/helpers.js
@@ -140,3 +140,23 @@ function changeOperator(signalGraph, node, operation){
     var newCode = previousCode.replace(/\+|\/|\-|\*/g,operation);
     code.innerHTML = code.innerHTML.replace(previousCode,newCode);
 }
+
+var decodeEntities = (function() {
+  // this prevents any overhead from creating the object each time
+  var element = document.createElement('div');
+
+  function decodeHTMLEntities (str) {
+    if(str && typeof str === 'string') {
+      // strip script/html tags
+      str = str.replace(/<script[^>]*>([\S\s]*?)<\/script>/gmi, '');
+      str = str.replace(/<\/?\w(?:[^"'>]|"[^"]*"|'[^']*')*>/gmi, '');
+      element.innerHTML = str;
+      str = element.textContent;
+      element.textContent = '';
+    }
+
+    return str;
+  }
+
+  return decodeHTMLEntities;
+})();
