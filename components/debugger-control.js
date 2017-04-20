f29314b702f848c44adddfd2b11b316c5352e3ac
Bruno ROCHA PEREIRA <brochape@vub.ac.be>
2017-04-20 11:30:30 +0200

    [Add] Working play/pause buttons

--- components/debugger-control.js
+++ components/debugger-control.js
@@ -1,6 +1,27 @@
 AFRAME.registerComponent('debugger-control', {
     init: function () {
-        this.el.addEventListener("click", () => console.log("coucou"))
+        var counter = 0
+        this.el.addEventListener("click", (evt) => {
+            counter +=1;
+            if (counter%2==1) {
+                console.log(this.el.getAttribute('id'))
+                
+                switch (this.el.getAttribute('id')) {
+                    case 'play':
+                        this.el.emit('startDebugger')
+                        break;
+                    case 'pause':
+                        this.el.emit('stopDebugger');
+                        break;
+                    case 'restart':
+                        break;
+                    default:
+                        // statements_def
+                        break;
+                }
+            }
+        })
+
     }
 });
 
